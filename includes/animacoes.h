f270fe2dac5d22d6ff4c62f5ca1e917494228742
DorivaldoJr <dorivaldojsr@gmail.com>
2025-01-25 22:02:59 -0300

    Adicionei a funcao para a animacao de uma seta girando em 360 graus

--- includes/animacoes.h
+++ includes/animacoes.h
@@ -8,5 +8,5 @@ void animacao_coracao(PIO pio, uint sm);
 void animacao_cruz(PIO pio, uint sm);
 void animacao_balada(PIO pio, uint sm);
 void animacao_emoji(PIO pio, uint sm);
-
+void animacao_seta(PIO pio, uint sm);
 #endif // ANIMACOES_H
